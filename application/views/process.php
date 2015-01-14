248040b926d44191c3a479ac122b062419cda24f
fahmid <fahmid789@clouditbd.com>
2015-01-14 18:33:41 +0600

    sds

---
+++ application/views/process.php
@@ -0,0 +1,175 @@
+<!--SLIDER-->
+<section class="innerpages">
+    <div class="grid_12">                       
+        <h1>Services</h1>
+    </div>
+</section>
+<!--SLIDER_END--> 
+</div>
+</div>
+<div class="block2">
+    <div class="container_12"> 
+        <!--==============================content================================-->
+        <section class="content">
+            <div class="wrapper">
+                <article class="grid_12 last-col">
+                    <div class="hello_box"> We create things out of nothing, solve problems & get stuff done.  Having worked with both small startups and large agencies, we know what it takes for ideas to come to life. </div>
+                </article>
+            </div>
+            <div class="wrapper">
+                <article class="grid_3">
+                    <h2 >Skills</h2>
+                    <p> It's very important to have a clear, visible and coherent identity to let your stakeholders know what you're all about. We can help you develop this. </p>
+                    <div class="clear"></div>
+                    <a href="<?php echo site_url('home/contact'); ?>" class="arrowlink">Contact Now</a> 
+                </article>
+                <div class="row clearfix">
+                    <article class="grid_9">
+                        <h1 id="headingtitle">Our Process</h1>
+                        <p>We love helping people. And we take pride in giving our clients an effective, high-quality product.</p>
+                        <ul class="our_process">
+                            <li class="img1">
+                                <h4>Step1: Planning</h4>
+                                <p>
+                                    This phase is the main focus of the project managers and stake holders.After requirement gathering these requirements are analyzed for their validity and the possibility of incorporating the requirements in the system to be development. <br>
+                                </p>
+                            </li>
+                            <li class="img2">
+                                <h4>Step2: Designing</h4>
+                                <p>
+                                    In this phase the system and software design is prepared from the requirement specifications which were studied in the first phase. System Design helps in specifying hardware and system requirements and also helps in defining overall system architecture. The system design specifications serve as input for the next phase of the model.
+                                </p>
+                            </li>
+                            <li class="img3">
+                                <h4>Step3: Development</h4>
+                                <p>
+                                    On receiving system design documents, the work is divided in modules/units and actual coding is started. Since, in this phase the code is produced so it is the main focus for the developer. This is the longest phase of the software development life cycle.
+                                </p>
+                            </li>    
+                            <li class="img3">
+                                <h4>Step3: Data Management & Processing</h4>
+                                <p>
+                                    Document management and file processing.
+                                </p>
+                            </li>
+                            <li class="img4">
+                                <h4>Step4: Testing</h4>
+                                <p>
+                                    After the code is developed it is tested against the requirements to make sure that the product is actually solving the needs addressed and gathered during the requirements phase. During this phase unit testing, integration testing, system testing, acceptance testing are done.
+                                </p>
+                            </li>
+                            <li class="img4">
+                                <h4>Step5: Delivery</h4>
+                                <p>
+                                    After successful testing the product is delivered / deployed to the customer for their use.
+                                </p>
+                            </li>
+                            <li class="img4">
+                                <h4>Step6: Maintenance</h4>
+                                <p>
+                                    Once when the customers starts using the developed system then the actual problems comes up and needs to be solved from time to time. This process where the care is taken for the developed product is known as maintenance.   
+                                </p>
+                            </li>
+                        </ul>
+                    </article>
+                </div>
+                <div class="clear"></div>
+                <div class="row clearfix">
+                    <article class="grid_12">
+                        <div class="quote-icon"><img src="<?php echo $baseurl; ?>assets/images/quote.png" alt="Quote"></div>
+                        <div class="pagination quotes-pagi" id="pager"> </div>
+                        <div class="quote">
+                            <div id="quotes">
+                                <p>We are delighted with the work done by HONESTY SOFTWARE. Their freshness, confidence and energy create a
+                                    wonderful, collaborative atmosphere. Not only do they provide solid, considered and inspired 
+                                    solutions that are a step ahead of the rest, but they really got involved with us on a 
+                                    personal level to ensure that we stay satisfied with results. </p>
+                                <p>We are delighted with the work done by honesty software. Their freshness, confidence and energy create a
+                                    wonderful, collaborative atmosphere. </p>
+                                <p>We are delighted with the work done by honesty software. Their freshness, confidence and energy create a
+                                    wonderful, collaborative atmosphere. Not only do they provide solid, considered and inspired 
+                                    solutions that are a step ahead of the rest, but they really got involved with us on a 
+                                    personal level to ensure that we stay satisfied with results. </p>
+                                <p>We are delighted with the work done by honesty software. </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+<div class="block3"> 
+    <!--==============================footer=================================-->
+    <div class="container_12">
+        <footer>
+            <div class="wrapper">
+                <div class="grid_5">
+                    <h2>About</h2>
+                    <p>Hi, we are Honesty Software - a creative agency! 
+                        Honesty Software emphasizes clean displays of content while facilitating great user experiences.
+                    </p>
+                    <ul class="social">
+                        <li> <a href="#">
+                                <figure><img src="<?php echo $baseurl; ?>assets/images/soc1.png" width="31" height="24" alt=""></figure>
+                                Follow us on Twitter</a> 
+                        </li>
+                        <li> <a href="https://www.facebook.com/honestysoft">
+                                <figure><img src="<?php echo $baseurl; ?>assets/images/soc2.png" width="31" height="26" alt=""></figure>
+                                Join us on Facebook</a> 
+                        </li>
+                        <li> <a href="#">
+                                <figure><img src="<?php echo $baseurl; ?>assets/images/soc3.png" width="31" height="26" alt=""></figure>
+                                Subscribe to our blog</a> 
+                        </li>
+                        <li class="cont_item m_bottom_zero"> <a href="index-5.html">
+                                <figure><img src="<?php echo $baseurl; ?>assets/images/soc4.png" width="31" height="20" alt=""></figure>
+                                Contact Us</a> 
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="grid_3 last-col">
+                    <h2>Meta</h2>
+                    <ul class="navigate">
+                        <li class="current"><a href="<?php echo site_url('home'); ?>">Home</a></li>
+                        <li><a href="<?php echo site_url('home/aboutus'); ?>">About us</a></li>
+                        <li><a href="<?php echo site_url('home/services'); ?>">Services</a></li>
+                        <li><a href="<?php echo site_url('home/hosting'); ?>">Hosting</a></li>
+                        <li><a href="<?php echo site_url('home/projectfourcol'); ?>">Projects</a></li>                                                      
+                        <li><a href="<?php echo site_url('home/blog'); ?>">Blog</a></li>          
+                        <li><a href="<?php echo site_url('home/contact'); ?>">Contact</a></li>
+                        <li><a href="#">Site Map</a></li>  
+                    </ul>
+                </div>
+                <div class="grid_4">
+                    <div id="fb-root"></div>
+                    <script>(function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id))
+                                return;
+                            js = d.createElement(s);
+                            js.id = id;
+                            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=373650309375413&version=v2.0";
+                            fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script', 'facebook-jssdk'));
+                    </script>
+                    <div class="fb-like-box" style="background-color: white" data-href="https://www.facebook.com/honestysoft" data-width="300px" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div> 
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery-1.8.2.min.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/superfish.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery.responsivemenu.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery.flexslider.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery.easing.1.3.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery.isotope.min.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/contact.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery.carouFredSel-6.1.0-packed.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/touchTouch.jquery.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery-ui-1.8.24.custom.min.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/script.js"></script>
+</body>
+</html>
