210a595132ab7ca746282742bc80a64fedbf776b
fahmid <fahmid789@clouditbd.com>
2015-01-18 17:41:21 +0600

    sdcvadv

---
+++ application/views/career.php
@@ -0,0 +1,135 @@
+<!--SLIDER-->
+<section class="innerpages">
+    <div class="grid_12">                       
+        <h1>Career</h1>
+    </div>
+</section>
+<!--SLIDER_END--> 
+</div>
+</div>
+<div class="block2">
+    <div class="container_12"> 
+        <!--==============================content================================-->
+        <section class="content">
+            <div class="wrapper">
+                <article class="grid_12 last-col">
+                    <div class="hello_box"> We create things out of nothing, solve problems & get stuff done.  Having worked with both small startups and large agencies, we know what it takes for ideas to come to life. </div>
+                </article>
+            </div>
+            <div class="wrapper">                
+                <article class="grid_3">
+                    <h2 >Skills</h2>
+                    <p> It's very important to have a clear, visible and coherent identity to let your stakeholders know what you're all about. We can help you develop this. </p>
+                    <div class="clear"></div>
+                    <a href="<?php echo site_url('home/contact'); ?>" class="arrowlink">Contact Now</a> </article>
+                <article class="grid_9">
+                    <h1 id="headingtitle">Our Career</h1>
+                    <p>We love helping people. And we take pride in giving our clients an effective, high-quality product.</p>
+                    <div class="row clearfix">
+                        <article class="grid_9 alpha">
+                            <div class="feature">
+                                <div class="feature-circle red"> <img src="<?php echo $baseurl; ?>assets/images/heart.png" alt=""></div>
+                                <div class="feature-description">
+                                    <h4>WHY HONESTY SOFTWARE</h4>
+                                    <p> Our employees are strongly encouraged to use their own initiative and to be flexible. They are given the freedom to shape their roles and to define areas of work that will benefit directly from their individual expertise. This democratic approach encourages open discussion and brings new ideas to the table, ultimately creating a startup advantage.
+                                        The startup advantage enables employees to view themselves as entrepreneurs and change makers. They take pride in their ideas delivering value to customers, thereby transforming the business landscape. </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="row clearfix">
+                        <article class="grid_9">
+                            <div class="feature">
+                                <div class="feature-circle red"> <img src="<?php echo $baseurl; ?>assets/images/formobile.png" alt=""></div>
+                                <div class="feature-description">
+                                    <h4>WHAT WE DO</h4>
+                                    <p> At HST, we recruit across a wide spectrum of roles from consulting to technology specialists where you can identify opportunities to live your ideas. There are numerous opportunities for you to work in projects related to research, product development, etc. across various industry segments and industry specific solutions.
+
+                                        We believe that in recognizing our employees as entrepreneurs and empowering them to innovate, we harness your entrepreneurial energy. If you are looking to let your start-up spirit soar free and your passion lies in solving complex business problems, then HST is the exact place for you. At HST, we bring IT and engineering services under one roof to solve complex business problems for our clients. We provide a holistic, multi-service delivery across various industries by leveraging our global offshore infrastructure. Look at how our structured service lines offer solutions that are sustainable and innovative.
+
+                                        Induction training is an important component of the training life cycle. Induction Training for new hires is vital to ensure that they settle in well in the organization. Our aim is to ascertain that new hires understand our organization‘s mission, goals, values and philosophy; and importantly their job role as expected in a professional environment. This training aims to establish a clear foundation and set expectations around work ethics, integrity and responsibility. With this in mind, we have customized our training to suit different levels i.e. Entry Level hires and Lateral hires.
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </article>                       
+                <div class="clear"></div>
+            </div>
+        </section>
+    </div>
+</div>
+<div class="block3"> 
+    <!--==============================footer=================================-->
+    <div class="container_12">
+        <footer>
+            <div class="wrapper">
+                <div class="grid_5">
+                    <h2>About</h2>
+                    <p>Hi, we are Honesty Software - a creative agency! 
+                        Honesty Software emphasizes clean displays of content while facilitating great user experiences.
+                    </p>
+                    <ul class="social">
+                        <li> <a href="#">
+                                <figure><img src="<?php echo $baseurl; ?>assets/images/soc1.png" width="31" height="24" alt=""></figure>
+                                Follow us on Twitter</a> 
+                        </li>
+                        <li> <a href="https://www.facebook.com/honestysoft">
+                                <figure><img src="<?php echo $baseurl; ?>assets/images/soc2.png" width="31" height="26" alt=""></figure>
+                                Join us on Facebook</a> 
+                        </li>
+                        <li> <a href="#">
+                                <figure><img src="<?php echo $baseurl; ?>assets/images/soc3.png" width="31" height="26" alt=""></figure>
+                                Subscribe to our blog</a> 
+                        </li>
+                        <li class="cont_item m_bottom_zero"> <a href="index-5.html">
+                                <figure><img src="<?php echo $baseurl; ?>assets/images/soc4.png" width="31" height="20" alt=""></figure>
+                                Contact Us</a> 
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="grid_3 last-col">
+                    <h2>Meta</h2>
+                    <ul class="navigate">
+                        <li class="current"><a href="<?php echo site_url('home'); ?>">Home</a></li>
+                        <li><a href="<?php echo site_url('home/aboutus'); ?>">About us</a></li>
+                        <li><a href="<?php echo site_url('home/services'); ?>">Services</a></li>
+                        <li><a href="<?php echo site_url('home/hosting'); ?>">Hosting</a></li>
+                        <li><a href="<?php echo site_url('home/projectfourcol'); ?>">Projects</a></li>                                                      
+                        <li><a href="<?php echo site_url('home/blog'); ?>">Blog</a></li>          
+                        <li><a href="<?php echo site_url('home/contact'); ?>">Contact</a></li>
+                        <li><a href="#">Site Map</a></li>  
+                    </ul>
+                </div>
+                <div class="grid_4">
+                    <div id="fb-root"></div>
+                    <script>(function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id))
+                                return;
+                            js = d.createElement(s);
+                            js.id = id;
+                            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=373650309375413&version=v2.0";
+                            fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script', 'facebook-jssdk'));
+                    </script>
+                    <div class="fb-like-box" style="background-color: white" data-href="https://www.facebook.com/honestysoft" data-width="300px" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div> 
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery-1.8.2.min.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/superfish.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery.responsivemenu.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery.flexslider.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery.easing.1.3.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery.isotope.min.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/contact.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery.carouFredSel-6.1.0-packed.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/touchTouch.jquery.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/jquery-ui-1.8.24.custom.min.js"></script> 
+<script type="text/javascript" src="<?php echo $baseurl; ?>assets/js/script.js"></script>
+</body>
+</html>
